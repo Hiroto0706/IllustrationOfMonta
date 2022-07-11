10b116167ef3dc31b4dab30398e5e7eefeb04e85
Hiroto0706 <mirunina2366@icloud.com>
2022-07-11 18:42:17 +0900

    cssを少し変更しました

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$(function(){
+$(function() {
+  const ham = $('.hamburger');
+  const nav = $('.navi');
+  ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+    ham.toggleClass('active'); 
+    nav.toggleClass('active'); 
+  });
 
+  $('.navi a').on('click', function(){
+    if(nav.hasClass('active')){
+      ham.toggleClass('active'); 
+      nav.toggleClass('active'); 
+    }
+  })
+
+  
 });
